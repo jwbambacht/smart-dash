ec9435f36e6c278f19f50f0b3739d53848bc87a4
Joost <jbambacht@me.com>
2022-10-13 20:48:46 +0200

    create serviceLoader

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,8 @@ import { useContainer } from "typeorm";
 
 import { seedDB, setupTypeORM } from './loaders/typeORMLoader';
 import { setupExpressApp } from "./loaders/expressLoader";
+import { initializeServices } from './loaders/serviceLoader';
 import { LoggerService } from "./services/LoggerService";
-import { SocketService } from "./services/SocketService";
-import { DevicesService } from "./services/DevicesService";
-import { WeatherService } from "./services/WeatherService";
-import { MapService } from "./services/MapService";
-import { NSService } from "./services/NSService";
-import { CalendarService } from "./services/CalendarService";
-import { TaskService } from "./services/TaskService";
-import { SpotifyService } from "./services/SpotifyService";
 
 // Set up the typeorm and typedi integration
 useContainer(Container);
@@ -24,20 +17,11 @@ dotenv.config();
 // Obtain logger from container
 const logger = Container.get(LoggerService);
 
-// Setup database connection and express app and initialize all services
+// Setup database connection, express app and initialize all services
 setupTypeORM()
     .then(async () => await seedDB(logger))
-    .then(() => setupExpressApp(logger))
-    .then((io) => {
-        Container.get(SocketService).setServer(io);
-        Container.get(WeatherService);
-        Container.get(MapService);
-        Container.get(NSService);
-        Container.get(DevicesService);
-        Container.get(CalendarService);
-        Container.get(TaskService);
-        Container.get(SpotifyService);
-    })
+    .then(async () => await setupExpressApp(logger))
+    .then((io) => initializeServices(io))
     .catch((e) => {
         logger.error("An error occurred while starting the application.");
         logger.error(e);
---
+++ src/loaders/serviceLoader.ts
@@ -0,0 +1,24 @@
+import { Server } from 'socket.io';
+import { Container } from 'typedi';
+
+import { SocketService } from '../services/SocketService';
+import { SpotifyService } from '../services/SpotifyService';
+import { WeatherService } from '../services/WeatherService';
+import { MapService } from '../services/MapService';
+import { NSService } from '../services/NSService';
+import { DevicesService } from '../services/DevicesService';
+import { CalendarService } from '../services/CalendarService';
+import { TaskService } from '../services/TaskService';
+
+export function initializeServices(io: Server): void {
+	Container.get(SocketService).setServer(io);
+	Container.get(SpotifyService);
+	Container.get(WeatherService);
+	Container.get(MapService);
+	Container.get(NSService);
+	Container.get(DevicesService);
+	Container.get(CalendarService);
+	Container.get(TaskService);
+
+	return;
+}
